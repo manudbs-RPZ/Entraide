58264a57387052f2be7dc4e6d2afd524bc7bfb40
victoraii <victor.moin@hei.yncrea.fr>
2017-01-24 00:32:41 +0100

    Commit build.gradle, app package, AndroidManifest

---
+++ app/src/main/java/vimo/entraide/app/AppConfig.java
@@ -0,0 +1,13 @@
+package vimo.entraide.app;
+
+/**
+ * Created by Victor Moin on 24/01/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.83.1/entraide/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.83.1/entraide/register.php";
+}
