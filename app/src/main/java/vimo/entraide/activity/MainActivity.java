45e72a5258bd1cf7dbf0619fcd764ec9d9f6c7d3
Manu le plus beau <emmanuel.de-bussac@hei.yncrea.fr>
2017-01-26 19:16:29 +0100

    Menu fonctionnel avec bar de nav

--- app/src/main/java/vimo/entraide/activity/MainActivity.java
+++ app/src/main/java/vimo/entraide/activity/MainActivity.java
@@ -11,8 +11,10 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,75 +22,67 @@ public class MainActivity extends AppCompatActivity{
 
 
 
+    /*Creation du burger */
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    /*fin*/
 
+    private Toolbar toolbar;
 
-    private TextView txtName;
-    private TextView txtEmail;
-    private Button btnLogout;
-
-    private SQLiteHandler db;
-    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
-
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
 
-        txtName = (TextView) findViewById(R.id.name);
-        txtEmail = (TextView) findViewById(R.id.email);
-        btnLogout = (Button) findViewById(R.id.btnLogout);
+        /*Creation de la bare du haut*/
+        toolbar= (Toolbar) findViewById(R.id.nav_bar);
+        setSupportActionBar(toolbar);
 
-        // SqLite database handler
-        db = new SQLiteHandler(getApplicationContext());
+        /*Creation burger (ico menu)*/
 
-        // session manager
-        session = new SessionManager(getApplicationContext());
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle= new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
-        if (!session.isLoggedIn()) {
-            logoutUser();
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /* fin creation burger */
+    }
+
+
+     /* Pour permetre d'ouvrir le menu */
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_acceuil) {
+
+        } else if (id == R.id.nav_acount) {
+
+        } else if (id == R.id.nav_logout) {
+
+
+
+        } else if (id== R.id.nav_publications){
+
         }
 
-        // Fetching user details from sqlite
-        HashMap<String, String> user = db.getUserDetails();
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
 
-        String name = user.get("name");
-        String email = user.get("email");
-
-        // Displaying the user details on the screen
-        txtName.setText(name);
-        txtEmail.setText(email);
-
-        // Logout button click event
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                logoutUser();
-            }
-        });
+        return super.onOptionsItemSelected(item);
     }
 
 
 
-    /**
-     * Logging out the user. Will set isLoggedIn flag to false in shared
-     * preferences Clears the user data from sqlite users table
-     * */
-    private void logoutUser() {
-        session.setLogin(false);
 
-        db.deleteUsers();
-
-        // Launching the login activity
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
 }
